6ddf8b798128221da66ab34744c5270377a878e2
Quackers <jameslmatthewsiii@gmail.com>
2018-10-09 01:19:51 -0400

    Trying out example for bot commands

--- pensbot.py
+++ pensbot.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+# -*- coding: UTF-8 -*-
 
 import os
-import discord
-import asyncio
 import configparser
 
-client = discord.Client()
+import discord
+import asyncio
+
+from discord.ext import commands
+
+
+config = configparser.ConfigParser()
+config.read(os.path.expanduser('~/.discord/credentials.ini'))
+
+'''client = discord.Client()
 
 @client.event
 async def on_ready():
@@ -27,9 +35,52 @@ async def on_message(message):
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
-
-
-config = configparser.ConfigParser()
-config.read(os.path.expanduser('~/.discord/credentials.ini'))
+    elif message.content.startswith('!f '):
+        await client.send_message(message.channel, 'Fuck {}!'.format(message.content.replace('!f ', '')))
+        await client.send_message(message.author, 'Did I make you happy, oWo?')
+        await client.delete_message(message)
+    elif message.content.startswith('!emoji'):
+        print('\U0001F32D')
+        await client.add_reaction(message, '\U0001F32D')
+    elif 'kessel' in message.content.lower():
+        await client.add_reaction(message, '\U0001F32D')
 
 client.run(config['DEFAULT']['token'])
+'''
+
+bot = commands.Bot(command_prefix='!', description='Yaas')
+
+@bot.event
+async def on_ready():
+    print('Fuck yeah!')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def add(left: int, right: int):
+    await bot.say(left + right)
+
+@bot.command()
+async def fuck(target: str):
+    await bot.say('Fuck {}!'.format(target))
+
+@bot.command()
+async def joined(member : discord.Member):
+    """Says when a member joined."""
+    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
+
+@bot.group(pass_context=True)
+async def cool(ctx):
+    """Says if a user is cool.
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
+
+@cool.command(name='bot')
+async def _bot():
+    """Is the bot cool?"""
+    await bot.say('Yes, the bot is cool.')
+
+bot.run(config['DEFAULT']['token'])
